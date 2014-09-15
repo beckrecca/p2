1d518b887fc7257d1d325714343332fadbf661f2
Rebecca Doris <rebecca.doris42@gmail.com>
2014-09-14 21:20:23 -0400

    Fixed null referent problems, added Bootstrap, added more words

--- index.php
+++ index.php
@@ -1,40 +1,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-
-    <title>XKCD password generator</title>
-
+    <meta charset="utf-8">
+    <title>xkcd password generator</title>
     <?php include 'logic.php' ?>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <style type="text/css">
+        #password { font-weight: bold;}
+    </style>
 </head>
 
 <body>
 
-<p>Insert an explanation of the app here.</p>
+<div class="container">
 
-<form method='POST' action='index.php'>
-    How many words would you like to use? <br>
-    <input type="radio" value="3" name="number_of_words">3
-    <input type="radio" value="4" name="number_of_words">4 
-    <input type="radio" value="5" name="number_of_words">5
-    <input type="radio" value="6" name="number_of_words">6 <br>
-    Would you like to include a number? 
-    <select name="incl_number">
-    <option value="0">No</option> number_of_words
-    <option value="1">Yes</option></select><br>
-    Would you like to include a character? 
-    <select name="incl_char">
-    <option value="0">No</option> 
-    <option value="1">Yes</option></select><br>
-    Would you like to capitalize the first letter?
-    <select name="cap">
-    <option value="0">No</option> 
-    <option value="1">Yes</option></select><br>
-    <input type='submit'>
-</form>
+    <p>Insert an explanation of the app here.</p>
 
-<?php 
-    echo $password;
- ?>
+    <form method="POST" action="index.php">
+        How many words would you like to use?
+        <input type="radio" value="3" name="number_of_words">3
+        <input type="radio" value="4" name="number_of_words">4 
+        <input type="radio" value="5" name="number_of_words">5
+        <input type="radio" value="6" name="number_of_words">6 <br>
+        Would you like to include a number? 
+        <select name="incl_number">
+        <option value="0">No</option> number_of_words
+        <option value="1">Yes</option></select><br>
+        Would you like to include a character? 
+        <select name="incl_char">
+        <option value="0">No</option> 
+        <option value="1">Yes</option></select><br>
+        Would you like to capitalize the first letter?
+        <select name="cap">
+        <option value="0">No</option> 
+        <option value="1">Yes</option></select><br>
+        <input type="submit" class="btn btn-default">
+    </form>
 
+    <p>
+        Your password is <span id="password"><?php echo $password; ?></span>
+    </p>
+
+    <?php echo count($words); ?>
+</div>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,18 +1,39 @@
 <?php
-	# Initialize an array of words from which to select.
-	$words = array("puppy", "eyeliner", "couch", "skyline", "inordinate", "cool", "shimmering", "skeleton", "blue", "binder", "hello", "monday", "sandwich", "pamphlet", "under", "hard", "chair", "purple", "monkey", "dishwasher", "hidden", "misunderstood", "prayer", "rolling", "mossy", "separate", "bright", "television", "chocolate", "strawberry", "spoon", "mirror", "incomplete", "long", "test", "stubborn", "mutt", "golem", "from", "gift", "terrible", "comfort", "mountain", "bookshelf", "curly", "escape", "ceiling", "glass", "parachute", "orange", "lime", "door", "thorough", "wandering", "iphone", "smile", "sniffs", "colorless", "green", "ideas", "dream", "furious");
+	# Initialize our variables.
+	$number_of_words;
+	$incl_number;
+	$incl_char;
+	$cap;
+
+	# If the user has submitted, set our variables according to
+	# the user's setting. Otherwise, set our variables to random
+	# values.
 
 	# Set the number of words to be used in the password.
-	$number_of_words = $_POST["number_of_words"];
+	if (isset($_POST["number_of_words"])) {
+		$number_of_words = $_POST["number_of_words"];}
+	else $number_of_words = rand(3,6);
 
 	# Set whether to include a number or not.
-	$incl_number = $_POST["incl_number"];
+	if (isset($_POST["incl_number"])) {
+		$incl_number = $_POST["incl_number"];
+	}
+	else $incl_number = rand(0,1);
 
 	# Set whether to include a special character or not.
-	$incl_char = $_POST["incl_char"];
+	if (isset($_POST["incl_char"])) {
+		$incl_char = $_POST["incl_char"];
+	}
+	else $incl_char = rand(0,1);
 
 	# Set whether to capitalize the first letter or not.
-	$cap = $_POST["cap"];
+	if (isset($_POST["cap"])) {
+		$cap = $_POST["cap"];
+	}
+	else $cap = rand(0,1);
+
+	# Initialize an array of words from which to select.
+	$words = array("puppy", "eyeliner", "couch", "heaven", "boots", "beautiful", "strong", "fever", "lust", "drunk", "pious", "enormous", "common", "town", "permit", "ticket", "above", "hammer", "thunder", "music", "note", "tune", "broom", "clean", "squeaky", "eggplant", "foliage", "rose", "pink", "virtuous", "special", "forgetful", "cat", "revenge", "iron", "knob", "diary", "dairy", "cake", "pie", "cheese", "bread", "stable", "friendly", "familiar", "awful", "informative", "teacher", "smooth", "mud", "wire", "hairy", "duck", "bananas", "decimated", "cream", "avocado", "feelings", "sedative", "fear", "keen", "cupcake", "frosting", "bubblegum", "princess", "hamster", "grumpy", "box", "dragon", "gold", "treasure", "adventure", "time", "evening", "saying", "troll", "frozen", "magic", "magical", "simple", "heavy", "many", "often", "sometimes", "difficult", "roommates", "baker", "pan", "knives", "giggling", "crawling", "baby", "silly", "goofy", "favorite", "elfish", "victorian", "fantasy", "inconceivable", "mathematical", "algebraic", "loving", "hugs", "smooches", "breeches", "underpants", "elbow", "knuckles", "bingo", "cabin", "lake", "kayak", "sunshine", "blonde", "bursting", "tiny", "adorable", "toxic", "wiener", "penguin", "apples", "stickers", "old", "elephant", "shapeshifting", "stretchy", "brave", "sword", "genius", "scientist", "beaker", "goggles", "conniving", "plan", "spontaneous", "ache", "complaints", "tips", "raspberry", "gush", "blood", "vampire", "unicorn", "rainbow", "cloud", "sky", "floating", "eternal", "best", "friend", "star", "twinkling", "drawbridge", "castle", "river", "waves", "roaring", "lion", "tiger", "kittens", "fuzzy", "cutest", "bottom", "uncut", "freckles", "rubbery", "porous", "even", "courageous", "segment", "ripped", "floppy", "sandy", "pale", "bronze", "timber", "tinder", "spark", "yeti", "ice", "king", "rotten", "wood", "foreign", "skyline", "inordinate", "cool", "shimmering", "skeleton", "blue", "binder", "hello", "monday", "sandwich", "pamphlet", "under", "hard", "chair", "purple", "monkey", "dishwasher", "hidden", "misunderstood", "prayer", "rolling", "mossy", "separate", "bright", "television", "chocolate", "strawberry", "spoon", "mirror", "incomplete", "long", "test", "stubborn", "mutt", "golem", "from", "gift", "terrible", "comfort", "mountain", "bookshelf", "curly", "escape", "ceiling", "glass", "parachute", "orange", "lime", "door", "thorough", "wandering", "iphone", "smile", "sniffs", "colorless", "green", "ideas", "dream", "furious");
 
 	# Initialize the password.
 	$password = generate_password ($number_of_words, $words);
